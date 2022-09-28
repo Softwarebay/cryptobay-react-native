c83e4e8d05f9d7b5b04964be538807d1f42f04fc
OskarPrzydatek <o.przydatek@wp.pl>
2022-09-28 11:36:44 +0200

    lint and format icon test

--- src/components/atoms/Icon/Icon.test.tsx
+++ src/components/atoms/Icon/Icon.test.tsx
@@ -9,10 +9,22 @@ import { Icons } from './icons.enum';
 
 describe('Icon Atom', () => {
   test('Component Snapshot', () => {
-    const walletIconTree = renderer
-      .create(<Icon type={Icons.WALLET} />)
-      .toJSON();
-    expect(walletIconTree).toMatchSnapshot('Wallet Icon');
+    const iconTree = (icon: Icons) =>
+      renderer.create(<Icon type={icon} />).toJSON();
+    expect(iconTree(Icons.ARROW_DOWN)).toMatchSnapshot('Arrow Down Icon');
+    expect(iconTree(Icons.ARROW_LEFT_BACK)).toMatchSnapshot(
+      'Arrow Left Back Icon',
+    );
+    expect(iconTree(Icons.ARROW_RIGHT_RECIVED)).toMatchSnapshot(
+      'Arrow Right Recived Icon',
+    );
+    expect(iconTree(Icons.ARROW_RIGHT_SENT)).toMatchSnapshot(
+      'Arrow Right Sent Icon',
+    );
+    expect(iconTree(Icons.BUY)).toMatchSnapshot('Buy Icon');
+    expect(iconTree(Icons.SETTINGS)).toMatchSnapshot('Settings Icon');
+    expect(iconTree(Icons.SWAP)).toMatchSnapshot('Swap Icon');
+    expect(iconTree(Icons.WALLET)).toMatchSnapshot('Wallet Icon');
   });
 
   test('ensure good icon renders by type', () => {
@@ -22,5 +34,10 @@ describe('Icon Atom', () => {
     expect(icon('wallet')).not.toBeNull();
     expect(icon('wallet')).toHaveProp('width', 24);
     expect(icon('wallet')).toHaveProp('height', 24);
+  });
+
+  test('ensure bad icon shall not render', () => {
+    render(<Icon type={'COFFE_CUP' as Icons} />);
+    expect(screen.queryByTestId('svg-icon-coffe-cup')).toBeNull();
   });
 });
