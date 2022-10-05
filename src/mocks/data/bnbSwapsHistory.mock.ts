2e578b105ed87fd18544998f8033696151de52af
OskarPrzydatek <o.przydatek@wp.pl>
2022-10-05 17:21:40 +0200

    add bnbSwapHistory mock and starts naming more detailed

---
+++ src/mocks/data/bnbSwapsHistory.mock.ts
@@ -0,0 +1,32 @@
+import { Swap } from "../../types/swap.type";
+
+export const bnbSwapsHistory = [
+  {
+    date: 'Mar 3 at 10:04am',
+    operation: 'Received',
+    status: 'Confirmed',
+    value: 0.04,
+    valueInDolar: 9.58799,
+  },
+  {
+    date: 'Mar 3 at 10:04am',
+    operation: 'Sent',
+    status: 'Cancelled',
+    value: 2.35,
+    valueInDolar: 547.5265,
+  },
+  {
+    date: 'Mar 3 at 10:04am',
+    operation: 'Received',
+    status: 'Confirmed',
+    value: 1.876,
+    valueInDolar: 436.11371,
+  },
+  {
+    date: 'Mar 3 at 10:04am',
+    operation: 'Sent',
+    status: 'Confirmed',
+    value: 0.04,
+    valueInDolar: 9.58799,
+  },
+] as Swap[];
--- src/mocks/data/etheriumTocken.mock.ts
+++ src/mocks/data/etheriumTocken.mock.ts
@@ -1,6 +1,6 @@
 import { Token } from '../../types/token.type';
 
-export const tokenMock = {
+export const etheriumTockenMock = {
   name: 'Etherium',
   shortName: 'ETH',
   price: 16858.15,
