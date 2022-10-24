881960ef1c03c0b83f194aeb23df80491265e049
OskarPrzydatek <o.przydatek@wp.pl>
2022-10-24 09:49:01 +0200

    add walthought screen test

---
+++ src/screens/WalthoughtScreen/WalthoughtScreen.test.tsx
@@ -0,0 +1,41 @@
+import { fireEvent, render, screen } from '@testing-library/react-native';
+import React from 'react';
+import '@testing-library/jest-native';
+import 'react-native';
+import renderer from 'react-test-renderer';
+
+import { Screens } from '../../screens.enum';
+
+import { WalthoughtScreen } from './WalthoughtScreen';
+
+const mockNavigation = {
+  navigate: jest.fn(),
+} as any;
+
+describe('WalthoughtScreen Screen', () => {
+  test('Component Snapshot', () => {
+    const tree = renderer.create(
+      <WalthoughtScreen
+        navigation={mockNavigation}
+        route={Screens.WALTHOUGHT as any}
+      />,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('ensure navigation works correctly', () => {
+    render(
+      <WalthoughtScreen
+        navigation={mockNavigation}
+        route={Screens.WALTHOUGHT as any}
+      />,
+    );
+    const carouselButton = screen.getByTestId('walthought-carousel-button');
+    fireEvent.press(carouselButton);
+    expect(mockNavigation.navigate).not.toHaveBeenCalled();
+    fireEvent.press(carouselButton);
+    expect(mockNavigation.navigate).not.toHaveBeenCalled();
+    fireEvent.press(carouselButton);
+    expect(mockNavigation.navigate).toHaveBeenCalledWith(Screens.WALLET_SETUP);
+  });
+});
