8b2d55a1fc3157e19bdcc4f9a05111b6281c367d
OskarPrzydatek <o.przydatek@wp.pl>
2022-10-28 13:00:52 +0200

    update profile avatar in storybook

--- src/components/molecules/ProfileAvatar/ProfileAvatar.stories.tsx
+++ src/components/molecules/ProfileAvatar/ProfileAvatar.stories.tsx
@@ -1,11 +1,13 @@
+import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react-native';
 import React from 'react';
 
-import { PROFILE } from '../../../../assets/pictures';
 import StorybookView from '../../../../storybook/StorybookView';
 
 import { ProfileAvatar } from './ProfileAvatar';
 
 storiesOf('Molecules/ProfileAvatar', module)
   .addDecorator(getStory => <StorybookView>{getStory()}</StorybookView>)
-  .add('Profile Avatar', () => <ProfileAvatar pictureFile={PROFILE} />);
+  .add('Profile Avatar', () => (
+    <ProfileAvatar onPressAvatar={action('onPressAvatar')} />
+  ));
